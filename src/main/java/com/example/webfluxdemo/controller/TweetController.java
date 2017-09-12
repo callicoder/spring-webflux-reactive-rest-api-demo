5f37b68faec1db2b982f599b99f005e2ce755df0
Rajeev Kumar Singh <rajeevs@flock.co>
2017-09-12 16:28:49 +0530

    Initial Commit

---
+++ src/main/java/com/example/webfluxdemo/controller/TweetController.java
@@ -0,0 +1,66 @@
+package com.example.webfluxdemo.controller;
+
+import com.example.webfluxdemo.model.Tweet;
+import com.example.webfluxdemo.repository.TweetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+
+/**
+ * Created by rajeevkumarsingh on 08/09/17.
+ */
+@RestController
+public class TweetController {
+
+    @Autowired
+    private TweetRepository tweetRepository;
+
+    @GetMapping("/tweets")
+    public Flux<Tweet> getAllTweets() {
+        return tweetRepository.findAll();
+    }
+
+    @PostMapping("/tweets")
+    public Mono<Tweet> createTweets(@Valid @RequestBody Tweet tweet) {
+        return tweetRepository.save(tweet);
+    }
+
+    @GetMapping("/tweets/{id}")
+    public Mono<Tweet> getTweetById(@PathVariable(value = "id") String reminderId) {
+        return tweetRepository.findById(reminderId);
+    }
+
+    @PutMapping("/tweets/{id}")
+    public Mono<ResponseEntity<Tweet>> updateTweet(@PathVariable(value = "id") String tweetId,
+                                                   @Valid @RequestBody Tweet tweet) {
+         return tweetRepository.findById(tweetId)
+                .flatMap(existingTweet -> {
+                    existingTweet.setText(tweet.getText());
+                    existingTweet.setCreatedAt(tweet.getCreatedAt());
+                    return tweetRepository.save(tweet);
+                })
+                 .map(updatedTweet -> new ResponseEntity<>(updatedTweet, HttpStatus.OK))
+                 .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/tweets/{id}")
+    public Mono<ResponseEntity<Void>> deleteTweet(@PathVariable(value = "id") String reminderId) {
+        return tweetRepository.findById(reminderId)
+                .flatMap(existingTweet -> tweetRepository.delete(existingTweet))
+                .then(Mono.just(new ResponseEntity<Void>(HttpStatus.OK)))
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    // Tweets are Sent to the client as Server Sent Events
+    @GetMapping(value = "/stream/tweets", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Tweet> streamAllTweets() {
+        return tweetRepository.findAll();
+    }
+
+}
---
+++ src/test/java/com/example/webfluxdemo/WebfluxDemoApplicationTests.java
@@ -0,0 +1,23 @@
+package com.example.webfluxdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class WebfluxDemoApplicationTests {
+
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@Test
+	public void contextLoads() {
+
+
+	}
+
+}
